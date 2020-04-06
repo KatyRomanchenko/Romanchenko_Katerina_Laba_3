fd43cba4d70a79fa3f525380859ea336a236c680
KatyRomanchenko <katy.romanchenko@gmail.com>
2020-04-06 21:49:29 +0300

    Laba3

---
+++ Laba_3.1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* ✔Дано текст, необхідно 
+ * ✔кожне слово (слова складаються з чисел і букв) помістити в колекцію, 
+ * ✔вивести загальну кількість елементів колекції, 
+ * ✔запросити ввести користувача порядковий номер елемента і напрямок руху по колекції, 
+ * ✔згідно введеному напрямку вивести всі слова починаючи з заданого.*/
+
+namespace Laba_3._1_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Text = "dfs21fsd sfwe12sdf s32fsf sfsdf2dfs 32fsdfs";
+            string[] words = Text.Split(' ');
+
+            var words_collection = new List<string>();
+            
+            foreach (string word in words)
+            {
+                words_collection.Add(word);
+            }
+            foreach (var word_ in words_collection)
+            {
+                Console.Write(word_ + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(words_collection.Count);
+            Console.Write("Введите число, с которого начнём перебор коллекции: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Введите в какую сторону двигаться(лево или право): ");
+            string answer = Console.ReadLine();
+            Console.WriteLine();
+            if (answer== "лево")
+            {
+                for (var index = number - 1; index >= 0; index--)
+                {
+                    Console.Write(words_collection[index] + " ");
+                }
+            }
+            else 
+            {
+                for (var index = number - 1; index < words_collection.Count; index++)
+                {
+                    Console.Write(words_collection[index] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba_3.2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*✔Дано словник. 
+ *✔Видалити пробіли в значеннях його ключів*/
+namespace Laba_3._2_0
+{
+    class Program
+    {
+        static void printDictionary(Dictionary<string, string> dictionary)
+        {
+            foreach (string c in dictionary.Keys)
+            {
+                Console.WriteLine("[" + c + "] =" + dictionary[c]);
+            }
+        }
+
+        static string myTrim(string s, string c)
+        {
+            string str = s.Replace(c, "");
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> smth = new Dictionary<string, string>(7);
+            Dictionary<string, string> tmp = new Dictionary<string, string>(7);
+            List<string> removals = new List<string>();
+
+            smth.Add("1", "smth1");
+            smth.Add("2", "smth2");
+            smth.Add("9   6", "smth3");
+            smth.Add(" 8  6", "smth4");
+            smth.Add("5", "smth5");
+            smth.Add("6", "smth6");
+            smth.Add("7", "smth7");
+
+            Console.WriteLine("before:");
+            printDictionary(smth);
+            
+            foreach (string c in smth.Keys)
+            {
+                string NewC = myTrim(c, " ");
+                tmp.Add(NewC, smth[c]);
+                removals.Add(c);
+            }
+
+            foreach (string c in removals)
+            {
+            smth.Remove(c);
+            }
+
+            foreach (string c in tmp.Keys)
+            {
+                smth.Add(c, tmp[c]);
+            }
+
+            Console.WriteLine("after:");
+            printDictionary(smth);
+
+            Console.ReadKey();
+        }
+    }
+}
